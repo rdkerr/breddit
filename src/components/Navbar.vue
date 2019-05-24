4425d9032be0ae782c4dabdd9842a967dd8c5918
rdkerr <rogerdkerr@gmail.com>
2019-05-24 14:37:15 -0700

    Frame for navbar, icon and text, responsive width

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="navbar">
+    <img src="../assets/breddit_logo.png" class="logo">
+    <div class="logo-text">
+      <p class="text">bredd<span class="i">i</span>t</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+@import "https://use.fontawesome.com/releases/v5.7.0/css/all.css";
+
+#navbar {
+  height: 48px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-bottom-color: rgb(237, 239, 241);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+  align-items: center;
+}
+.logo {
+  height: 32px;
+  width: 32px;
+  padding: 8px 8px 8px 0;
+  float: left;
+}
+.logo-text {
+  float: left;
+}
+.text {
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: -1px;
+  font-family: IBMPlexSans, Arial, sans-serif;
+  margin: 12px 20px 8px 0;
+}
+.i {
+  color: rgb(255, 69, 0);
+  position: relative;
+}
+.i:before {
+  content: "ı";
+  position: absolute;
+  color: black;
+}
+</style>
