78163f8fa6bdb95ecadbd7f039d4ef3ff89e92dd
rdkerr <rogerdkerr@gmail.com>
2019-05-24 15:08:42 -0700

    Responsive home icon

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,7 @@ export default {
 <style>
 body {
   margin: 0;
+  min-width: 320px;
 }
 
 #app {
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="navbar">
-    <img src="../assets/breddit_logo.png" class="logo">
-    <div class="logo-text">
-      <p class="text">bredd<span class="i">i</span>t</p>
+    <div class="home">
+      <img src="../assets/breddit_logo.png" class="logo">
+      <div class="logo-text">
+        <p class="text">bredd<span class="i">i</span>t</p>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +17,9 @@ export default {
 
 <style scoped>
 @import "https://use.fontawesome.com/releases/v5.7.0/css/all.css";
-
+.home {
+  display: inline-block;
+}
 #navbar {
   height: 48px;
   padding-left: 20px;
@@ -36,10 +40,16 @@ export default {
 }
 .text {
   font-size: 24px;
-  font-weight: 600;
+  font-weight: 500;
   letter-spacing: -1px;
   font-family: IBMPlexSans, Arial, sans-serif;
   margin: 12px 20px 8px 0;
+  display: none;
+}
+@media screen and (min-width: 1070px) {
+  .text {
+    display: block;
+  }
 }
 .i {
   color: rgb(255, 69, 0);
