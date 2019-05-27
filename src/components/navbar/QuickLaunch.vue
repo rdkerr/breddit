3ff739b183297a1287b9c77e1f3e9a1ec5a88652
rdkerr <rogerdkerr@gmail.com>
2019-05-26 22:45:10 -0700

    Finished QuickLaunch

--- src/components/navbar/QuickLaunch.vue
+++ src/components/navbar/QuickLaunch.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="launch">
-    <div class="icon-container">
-      <i class="fa fa-pepper-hot icon"></i>
+  <div>
+    <div class="launch">
+      <div class="icon-container">
+        <i class="fa fa-pepper-hot icon"></i>
+      </div>
+      <div class="icon-container">
+        <i class="fa fa-hamburger icon"></i>
+      </div>
+      <div class="icon-container">
+        <i class="fa fa-shopping-cart icon"></i>
+      </div>
     </div>
-    <div class="icon-container">
-      <i class="fa fa-pepper-hot icon"></i>
-    </div>
-    <div class="icon-container">
-      <i class="fa fa-pepper-hot icon"></i>
+    <div class="compose">
+      <div class="icon-container">
+        <i class="fa fa-envelope icon"></i>
+      </div>
+      <div class="icon-container">
+        <i class="fa fa-comment-dots icon"></i>
+      </div>
+      <div class="icon-container">
+        <i class="fa fa-scroll blue icon"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -21,22 +34,45 @@ export default {
 <style scoped>
 .launch {
   float: left;
-  max-width: 225px;
+  max-width: 112px;
   margin-top: 6px;
+  border-right: 1px solid rgb(230, 230, 230);
+  display: none;
+  padding: 0 5px;
+}
+.compose {
+  float: left;
+  max-width: 112px;
+  margin-top: 6px;
+  display: none;
+  padding: 0 5px;
 }
 .icon {
   font-size: 16px;
-  padding: 8px;
+  vertical-align: middle;
+  line-height: 36px;
 }
 .icon-container {
-  float: left;
-  width: 40px;
-  height: 36px;
-  margin-right: 8px;
-  background-color: blue;
+  display: inline-block;
+  width: 32px;
+  height: 32px;
+  border-radius: 4px;
+  text-align: center;
 }
 .icon-container:hover {
-  background-color: aquamarine;
+  background-color: rgba(26, 26, 27, 0.1);
 }
-
+.blue {
+  color: #0079d3;
+}
+@media screen and (min-width: 800px) {
+  .launch {
+    display: block;
+  }
+}
+@media screen and (min-width: 600px) {
+  .compose {
+    display: block;
+  }
+}
 </style>
