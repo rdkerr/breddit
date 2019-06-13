19083b6adf0c97e3fc297faffbe3b80aea2fcc94
rdkerr <rogerdkerr@gmail.com>
2019-06-13 13:43:35 -0700

    Fixed linting errors

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
 import Vue from 'Vue'
 import VueClickaway from 'vue-clickaway2'
 import App from './App.vue'
@@ -6,5 +9,5 @@ Vue.directive('clickaway', VueClickaway.directive)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: (h) => h(App),
 })
