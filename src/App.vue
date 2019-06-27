ffe97f949e21cfa2947743050e0bcd09e8fffe20
rdkerr <rogerdkerr@gmail.com>
2019-06-26 17:00:11 -0700

    Reorganized file structure

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,9 @@
 
 <script>
 import Navbar from "./components/navbar/Navbar"
-import Banner from './components/body/Banner'
+import Banner from './components/banner/Banner'
 import UtilityBar from './components/utilitybar/UtilityBar'
-import PostList from './components/body/PostList'
+import PostList from './components/post/PostList'
 
 export default {
   name: 'app',
